c2ffc669f75587181aa5153cb463cdde3a54b2a6
Huseyin Toprak <HuseyinToprak96@outlook.com>
2023-06-08 13:50:28 +0300

    aaa

--- react-haber-sitesi/src/components/root/App.js
+++ react-haber-sitesi/src/components/root/App.js
@@ -3,28 +3,11 @@ import Login from "../Login/Login";
 import NewsDetail from "../News/NewsDetail";
 import Dashboard from "./Dashboard";
 import {Route,Routes} from "react-router-dom"
-import {Container, Row, Col} from 'reactstrap';
-import Nav from "../Header/Nav";
-import Logo from "../Header/Logo";
+
 
 function App() {
   return (
     <>
-     <Dashboard/>
-    <Container className='m-0'>
-      <Row>
-        <Col xs="3">
-          <Logo/>
-        </Col>
-
-        <Col xs="9" className='m-auto'>
-          <Nav/>
-        </Col>
-
-      </Row>
-
-
-    </Container>
      <Routes>
 <Route path="/" element={<Login />} />
 <Route path="/home" element={<Dashboard />} />
--- react-haber-sitesi/src/components/root/Dashboard.js
+++ react-haber-sitesi/src/components/root/Dashboard.js
@@ -3,11 +3,23 @@ import Slider from "../News/Slider";
 import NewList from "../News/NewsList";
 import { Component } from "react";
 import {Col, Row, Container} from "reactstrap";
+import Nav from "../Header/Nav";
+import Logo from "../Header/Logo";
 export default class Dashboard extends Component {
     render() {
         return (
             <div>
                 <Container>
+                <Row>
+        <Col xs="3">
+          <Logo/>
+        </Col>
+
+        <Col xs="9" className='m-auto'>
+          <Nav/>
+        </Col>
+
+      </Row>
                     <Row>
                         <Col className="col-3">
                         <Categories/>
